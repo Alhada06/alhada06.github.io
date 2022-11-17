862bbda49b8c2421c6e084b39754882ecb0b565c
Diogo Alhada <93165102+Alhada06@users.noreply.github.com>
2022-11-17 14:54:07 +0000

    second test commit to see if tailwind works

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,7 +1,20 @@
+import { Static } from "nuxt/dist/app/compat/capi";
+
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
 export default defineNuxtConfig({
-    modules: [
-        '@nuxtjs/tailwindcss'
-      ]
+    
+    css: ['~/assets/css/main.css'],
+    postcss: {
+        plugins: {
+          tailwindcss: {},
+          autoprefixer: {},
+        },
+      },
+     
+   app:{
+    baseURL:'/alhada06.github.io/',
+    
+   },
+
 
 })
