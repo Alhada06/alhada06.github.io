3d210c9d7474167d02c302f7b4e6a02623387c0f
Diogo Alhada <93165102+Alhada06@users.noreply.github.com>
2022-11-17 16:22:39 +0000

    test 7 does it work properly now ?

--- app.vue
+++ app.vue
@@ -4,5 +4,6 @@
       hello from new start with tailwind installed changes made does it work?
     </div>
     <div class="text-white bg-blue-500">works or not ?</div>
+    <div class="text-red-900 bg-emerald-500">stil works???</div>
   </div>
 </template>
