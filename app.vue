6346bf427a97dbc45e58a6ccc150ea978b4bad78
Diogo Alhada <93165102+Alhada06@users.noreply.github.com>
2022-11-17 15:52:23 +0000

    test 2

--- app.vue
+++ app.vue
@@ -1,7 +1,7 @@
 <template>
   <div>
     <div class="text-blue-300">
-      hello from new start with tailwind installed
+      hello from new start with tailwind installed changes made does it work?
     </div>
   </div>
 </template>
