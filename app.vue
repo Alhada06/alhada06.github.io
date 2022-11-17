bac9de6cc03b62fe9d1ed11e4cb2b765bdf9f918
Diogo Alhada <93165102+Alhada06@users.noreply.github.com>
2022-11-17 15:55:04 +0000

    test 3

--- app.vue
+++ app.vue
@@ -3,5 +3,6 @@
     <div class="text-blue-300">
       hello from new start with tailwind installed changes made does it work?
     </div>
+    <div class="text-white bg-blue-500">works or not ?</div>
   </div>
 </template>
