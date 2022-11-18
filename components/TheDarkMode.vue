394ae4da94eb6cc8398e33d7d2e7fb6734a2bd7e
Diogo Alhada <93165102+Alhada06@users.noreply.github.com>
2022-11-18 23:11:04 +0000

    added transition from the icons on th darkmode toggle

--- components/TheDarkMode.vue
+++ components/TheDarkMode.vue
@@ -4,8 +4,27 @@ const toggleDark = useToggle(isDark);
 <template>
   <div>
     <button @click="toggleDark()" class="">
-      <Icon v-if="!isDark" name="material-symbols:nights-stay-rounded" />
-      <Icon v-else name="material-symbols:sunny-rounded" />
+      <Transition mode="out-in" name="slide-up">
+        <Icon v-if="!isDark" name="material-symbols:nights-stay-rounded" />
+        <Icon v-else name="material-symbols:sunny-rounded" />
+      </Transition>
     </button>
   </div>
 </template>
+
+<style>
+.slide-up-enter-active,
+.slide-up-leave-active {
+  transition: all 0.3s ease-out;
+}
+
+.slide-up-enter-from {
+  opacity: 0;
+  transform: translateY(10px);
+}
+
+.slide-up-leave-to {
+  opacity: 0;
+  transform: translateY(-10px);
+}
+</style>
