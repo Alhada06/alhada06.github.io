20b9d4b31434965ba15be9b3dbc682f7c8da0631
Alhada06 <93165102+Alhada06@users.noreply.github.com>
2023-03-13 16:38:33 +0000

    Deploying to gh-pages from @ Alhada06/alhada06.github.io@8f9db29917d2ecb9d2c99d73a947737279df4f37 🚀

---
+++ _nuxt/composables.5f6da9f2.js
@@ -0,0 +1,1 @@
+import{ab as a}from"./entry.c10d0570.js";function s(e,u){return a()._useHead(e,u)}export{s as u};
---
+++ _nuxt/config.a245ac2c.js
@@ -0,0 +1,1 @@
+import{ab as a,aa as n,ae as o}from"./entry.c10d0570.js";function f(){const p=a();return p._appConfig||(p._appConfig=n(o)),p._appConfig}export{f as u};
