6bb6fc5ce61fccab77cceb31d95f1d90c07a49f5
Diogo Alhada <93165102+Alhada06@users.noreply.github.com>
2023-03-15 17:50:21 +0000

    test session storage for future loading

--- app.vue
+++ app.vue
@@ -1,12 +1,37 @@
+<script setup lang="ts">
+const nuxtApp = useNuxtApp();
+const loading = ref(false);
+const isFirstRender = useSessionStorage("firstRender", true);
+nuxtApp.hook("app:mounted", () => {
+  if (isFirstRender.value) {
+    loading.value = true;
+  }
+
+  console.log("page mount");
+});
+
+nuxtApp.hook("page:finish", () => {
+  if (isFirstRender.value) {
+    loading.value = false;
+    isFirstRender.value = false;
+  }
+  console.log("page finished");
+});
+</script>
+
 <template>
+  <div v-if="loading" class="fixed left-0 top-10 h-2 w-full z-50 bg-red-500">
+    Loading{{ loading }}-- {{ isFirstRender }}
+  </div>
   <NuxtLayout>
     <!-- <div>
-      <div class="text-blue-300">
-        hello from new start with tailwind installed changes made does it work?
-      </div>
-      <div class="text-white bg-blue-500">works or not ?</div>
-      <div class="text-red-900 bg-emerald-500">stil works???</div>
-    </div> -->
+                                                      <div class="text-blue-300">
+                                                        hello from new start with tailwind installed changes made does it work?
+                                                      </div>
+                                                      <div class="text-white bg-blue-500">works or not ?</div>
+                                                      <div class="text-red-900 bg-emerald-500">stil works???</div>
+                                                    </div> -->
+
     <NuxtPage />
   </NuxtLayout>
 </template>
