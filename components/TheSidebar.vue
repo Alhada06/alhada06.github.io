e2222063c93e14f516ed6f7844a0fb70bf4cba8c
Diogo Alhada <93165102+Alhada06@users.noreply.github.com>
2023-03-22 16:27:06 +0000

    Fix sidebar background problem

--- components/TheSidebar.vue
+++ components/TheSidebar.vue
@@ -13,8 +13,8 @@ const router = useRouter();
 
 <template>
   <!-- route.fullPath === '/#' + index, -->
-<main class="grid grid-cols-6 gap-2 pt-8">
-  <Transition mode="out-in" :duration="400" name="fade">
+  <main class="grid grid-cols-6 gap-2 pt-8">
+    <Transition mode="out-in" :duration="400" name="fade">
       <aside v-if="isSidebarVisible" class="invisible md:visible w-full">
         <!-- <div class="sticky top-3 left-0 z-0 opacity-50">
             <div
@@ -28,15 +28,31 @@ const router = useRouter();
               class="z-0 mt-10 absolute border-t-[75px] opacity-60 border-t-transparent border-l-[155px] dark:border-l-blue-900 border-l-blue-300 border-b-[100px] border-b-transparent">
             </div>
           </div> -->
+        <div class="sticky top-3 left-0 z-0 opacity-50">
+          <div
+            class="z-0 absolute border-t-[75px] opacity-60 border-t-transparent border-l-[155px] dark:border-l-blue-900 border-l-blue-300 border-b-[100px] border-b-transparent"
+          ></div>
+          <div
+            class="z-0 mt-5 absolute border-t-[75px] opacity-80 border-t-transparent border-l-[155px] dark:border-l-blue-900 border-l-blue-300 border-b-[100px] border-b-transparent"
+          ></div>
+          <div
+            class="z-0 mt-10 absolute border-t-[75px] opacity-60 border-t-transparent border-l-[155px] dark:border-l-blue-900 border-l-blue-300 border-b-[100px] border-b-transparent"
+          ></div>
+        </div>
         <div class="sticky bg-opacity-70 rounded-l-lg top-5 left-0 pr-2 py-6">
           <div class="z-40">
-            <NuxtLink v-for="(header, index) in headers" :key="header" :to="'/#' + index"
-              class="block z-30 pl-2 m-1 w-fit dark:text-white text-blue-800 no-underline transition ease-in-out delay-150 hover:scale-110 dark:hover:text-blue-500 hover:text-blue-700 duration-300"
+            <NuxtLink
+              v-for="(header, index) in headers"
+              :key="header"
+              :to="'/#' + index"
+              class="block z-30 pl-2 m-1 w-fit dark:text-white text-blue-800 no-underline tracking-tight transition ease-in-out delay-150 hover:scale-110 dark:hover:text-blue-500 hover:text-blue-700 duration-300"
               :class="{
                 'border-blue-500 border-b-2 rounded-l-xl  border-solid':
                   isIntersectingSection['Section' + (index + 1)],
-              }">
-              {{ header }}</NuxtLink>
+              }"
+            >
+              {{ header }}</NuxtLink
+            >
           </div>
         </div>
       </aside>
