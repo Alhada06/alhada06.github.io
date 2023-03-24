95ce8a3607942acd15cdec67981818b6f039dc50
Diogo Alhada <93165102+Alhada06@users.noreply.github.com>
2023-03-24 18:09:55 +0000

    change to lang ts

--- pages/loading/index.vue
+++ pages/loading/index.vue
@@ -1,6 +1,6 @@
-<script setup>
+<script setup lang="ts">
 definePageMeta({
-    layout: "backgound",
+  layout: "backgound",
 });
 // setTimeout(() => {
 //     return navigateTo("/");
@@ -8,51 +8,52 @@ definePageMeta({
 // let sessionData =
 //     typeof window !== "undefined" ? localStorage.getItem("firstRender") : null;
 
-const percentage = ref(0);
-const percent = computed(() => percentage.value.toFixed());
+const percentage = ref<number>(0);
+const percent = computed<string>(() => percentage.value.toFixed());
 
 let timer = setInterval(() => {
-    if (percentage.value < 100) {
-        percentage.value += 0.1;
-    } else {
-        setTimeout(() => {
-            clearInterval(timer);
-            return navigateTo("/");
-        }, 1000);
-    }
+  if (percentage.value < 100) {
+    percentage.value += 0.1;
+  } else {
+    setTimeout(() => {
+      clearInterval(timer);
+      return navigateTo("/");
+    }, 1000);
+  }
 }, 3);
 </script>
 
 <template>
-    <div>
-        <div class="top-0 fixed left-0 w-full h-full">
-            <div class="flex relative justify-center items-center h-full z-10">
-                <div
-                    class="flex absolute inset-0 justify-center pl-2 items-center h-full dark:text-slate-100 text-blue-800 text-5xl z-20">
-                    {{ percent }}%
-                </div>
-                <div
-                    class="loader ease-linear rounded-full border-t-blue-800 dark:border-t-blue-800 border-8 border-t-8 dark:border-slate-100/70 border-slate-400/50 h-64 w-64">
-                </div>
-            </div>
+  <div>
+    <div class="top-0 fixed left-0 w-full h-full">
+      <div class="flex relative justify-center items-center h-full z-10">
+        <div
+          class="flex absolute inset-0 justify-center pl-2 items-center h-full dark:text-slate-100 text-blue-800 text-5xl z-20"
+        >
+          {{ percent }}%
         </div>
+        <div
+          class="loader ease-linear rounded-full border-t-blue-800 dark:border-t-blue-800 border-8 border-t-8 dark:border-slate-100/70 border-slate-400/50 h-64 w-64"
+        ></div>
+      </div>
     </div>
+  </div>
 </template>
 <style>
 .loader {
-    -webkit-animation: spinner 1.5s infinite;
-    animation: spinner 1.5s infinite;
+  -webkit-animation: spinner 1.5s infinite;
+  animation: spinner 1.5s infinite;
 }
 
 @-webkit-keyframes spinner {
-    to {
-        -webkit-transform: rotate(360deg);
-    }
+  to {
+    -webkit-transform: rotate(360deg);
+  }
 }
 
 @keyframes spinner {
-    to {
-        transform: rotate(360deg);
-    }
+  to {
+    transform: rotate(360deg);
+  }
 }
 </style>
