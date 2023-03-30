6796fa8d5151b1aaab7ef45c1c3f9036296821ee
Diogo Alhada <93165102+Alhada06@users.noreply.github.com>
2023-03-30 01:40:46 +0100

    basic implementation of web3forms for the contact from

--- components/TheContactForm.vue
+++ components/TheContactForm.vue
@@ -1,55 +1,35 @@
 <script setup>
-import sendgrid from "@sendgrid/mail";
 const runtimeConfig = useRuntimeConfig();
-
-if (process.server) {
-  sendgrid.setApiKey(runtimeConfig.NUXT_SENDGRID_API_KEY);
-}
 
 const form = ref({
   name: "",
   email: "",
   text: "",
+  access_key: "6214d9b4-c46f-4e98-9501-6e52ec230c79",
 });
 
 const submit = async () => {
   console.log(form.value);
-  const res = await useAsyncData("mail", () =>
-    $fetch("/api/mail", { method: "GET" })
+  const { data, error } = await useAsyncData("mail", () =>
+    $fetch("https://api.web3forms.com/submit", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        Accept: "application/json",
+      },
+      body: form.value,
+    })
   );
-  console.log(res);
+  console.log(data.value);
+  console.log(error.value);
 };
+
+const forms = ref(null);
+const result = ref(null);
 </script>
 
 <template>
-  <!-- <form @submit.prevent="submit">
-    <div class="flex flex-col p-1">
-      <div class="inline-flex p-2">
-        <label for="name">Name</label>
-        <input type="text" v-model="form.name" name="name" id="name" />
-      </div>
-      <div class="inline-flex p-2">
-        <label for="email">Email</label>
-        <input type="email" v-model="form.email" name="email" id="email" />
-      </div>
-      <div class="inline-flex p-2">
-        <label for="text">text</label>
-        <textarea
-          name="text"
-          id=""
-          cols="30"
-          v-model="form.text"
-          rows="10"
-        ></textarea>
-      </div>
-      <button class="bg-blue-700 hover:bg-blue-600 text-white" type="submit">
-        Send
-      </button>
-    </div>
-  </form> -->
-
-  <!-- component -->
-  <form class="w-full max-w-lg" @submit.prevent="submit">
+  <form ref="forms" id="forms" @submit.prevent="submit" class="w-full max-w-lg">
     <div class="flex flex-wrap -mx-3 mb-6">
       <div class="w-full px-3 mb-6 md:mb-0">
         <label
@@ -63,6 +43,7 @@ const submit = async () => {
           class="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white"
           id="grid-first-name"
           type="text"
+          name="name"
           placeholder="Jane"
           required
         />
@@ -82,6 +63,7 @@ const submit = async () => {
           class="appearance-none block w-full bg-gray-200/30 border border-gray-200 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white/40 focus:border-gray-500"
           id="email"
           type="email"
+          name="email"
           required
         />
         <p class="text-white text-xs italic">Some tips - as long as needed</p>
@@ -99,10 +81,12 @@ const submit = async () => {
           v-model="form.text"
           class="no-resize appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white focus:border-gray-500 h-48 resize-none"
           id="message"
+          name="message"
           required
         ></textarea>
       </div>
     </div>
+
     <div class="md:flex md:items-center md:justify-center">
       <button
         class="shadow bg-blue-700 w-1/2 hover:bg-blue-600 focus:shadow-outline focus:outline-none text-white font-bold py-2 px-4 rounded"
@@ -111,5 +95,6 @@ const submit = async () => {
         Send
       </button>
     </div>
+    <div ref="result" id="result"></div>
   </form>
 </template>
