5e8db8487ea491798141c9a3d218a79e84e35c42
Diogo Alhada <93165102+Alhada06@users.noreply.github.com>
2023-04-11 02:53:27 +0100

    fix certmodal alignment

--- components/TheCertsModal.vue
+++ components/TheCertsModal.vue
@@ -22,13 +22,13 @@ const props = defineProps({
     @close="props.onClose"
   >
     <!-- The backdrop, rendered as a fixed sibling to the panel container -->
-    <div class="fixed inset-0 bg-black/30" aria-hidden="true" />
+    <div class="fixed inset-0 bg-black/90" aria-hidden="true" />
 
     <!-- Full-screen container to center the panel -->
-    <div class="fixed inset-0 flex items-center justify-center p-4">
+    <div class="fixed inset-x-0 top-[15%] flex items-center justify-center p-4">
       <!-- The actual dialog panel -->
       <HeadlessDialogPanel
-        class="w-11/12 content-center rounded bg-white align-top dark:bg-bgbluelighter md:h-3/6 md:w-3/6"
+        class="w-11/12 max-w-4xl content-center rounded bg-white align-top dark:bg-bgbluelighter lg:md:h-3/6"
       >
         <HeadlessDialogTitle
           class="text-center text-blue-900 dark:text-white"
