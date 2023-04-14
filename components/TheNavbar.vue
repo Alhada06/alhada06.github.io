108dfc404ac73e974d788049007d7d9be5b6fa25
Diogo Alhada <93165102+Alhada06@users.noreply.github.com>
2023-04-15 00:22:58 +0100

    navbar css fix

--- components/TheNavbar.vue
+++ components/TheNavbar.vue
@@ -12,9 +12,11 @@ const route = useRoute();
 
 <template>
   <nav
-    class="fixed left-0 right-0 top-0 z-20 bg-slate-100 text-blue-800 shadow shadow-blue-900/10 dark:bg-bgblue dark:text-white dark:shadow dark:shadow-white/20 md:relative md:bg-inherit"
+    class="fixed left-0 right-0 top-0 z-20 bg-slate-100 text-blue-800 shadow shadow-blue-900/10 dark:bg-bgblue dark:text-white dark:shadow dark:shadow-white/20 md:relative md:bg-transparent md:shadow-transparent dark:md:bg-transparent dark:lg:md:shadow-none lg:md:shadow-none"
   >
-    <div class="mx-auto mb-2 mt-2 flex h-8 items-center justify-end px-2 py-3">
+    <div
+      class="mx-auto mb-2 mt-2 flex h-8 items-center justify-end bg-transparent px-2 py-3"
+    >
       <!-- <a class="text-2xl hover:text-cyan-500 transition-colors cursor-pointer">Logo</a> -->
 
       <ul class="flex items-center gap-1 pr-1 md:gap-5">
@@ -22,7 +24,7 @@ const route = useRoute();
           <li v-for="(header, index) in headers" :key="header">
             <NuxtLink
               :to="'/#' + index"
-              class="m-1 block pl-2 text-xs tracking-tighter text-blue-800 outline-offset-2 outline-blue-400 transition delay-150 duration-300 ease-in-out hover:scale-110 hover:text-blue-700 dark:text-white dark:outline-red-500 dark:hover:text-blue-500 md:text-base"
+              class="m-1 block pl-2 text-xs tracking-tighter text-blue-800 outline-offset-2 outline-blue-400 transition delay-150 duration-300 ease-in-out hover:scale-110 hover:text-blue-700 dark:text-white dark:hover:text-blue-500 md:text-base"
               :class="{
                 'underline decoration-blue-500 decoration-solid decoration-1 underline-offset-4':
                   isIntersectingSection['Section' + (index + 1)],
