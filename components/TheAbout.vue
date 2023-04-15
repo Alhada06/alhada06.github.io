c8e4d551b0ba1f2f1ef97ae25b9c88f8794d32ff
Diogo Alhada <93165102+Alhada06@users.noreply.github.com>
2023-04-15 04:25:32 +0100

    added about

--- components/TheAbout.vue
+++ components/TheAbout.vue
@@ -1,10 +1,20 @@
 <script setup>
 import aboutQuery from "@/graphql/queries/about.query.gql";
+import academicsQuery from "@/graphql/queries/academics.query.gql";
 
 const { locale } = useI18n();
 
 const { result: aboutData } = useQuery(
   aboutQuery,
+  () => {
+    return { locale: locale.value };
+  },
+  {
+    prefetch: false,
+  }
+);
+const { result: academicsData } = useQuery(
+  academicsQuery,
   () => {
     return { locale: locale.value };
   },
@@ -21,11 +31,41 @@ const { result: aboutData } = useQuery(
     >
       {{ aboutData?.about?.description }}
     </div>
-    <div class="flex flex-col">
+    <div class="m-1 flex w-full flex-col p-1">
       <div class="text-left text-lg text-blue-900 dark:text-white">
         {{ $t("academics") }}
       </div>
-      <div>aasfas asd as</div>
+      <div class="m-1 p-1 pt-4">
+        <div
+          v-for="(degree, index) in academicsData?.academicsCollection?.items"
+          :key="index"
+          class="mb-2 flex w-full flex-col pb-3"
+          :class="{
+            'border-b  border-blue-900/90 dark:border-slate-200/50':
+              index < academicsData?.academicsCollection.total - 1,
+          }"
+        >
+          <div
+            class="m-1 whitespace-pre-wrap p-1 text-justify text-sm tracking-tighter text-blue-900/95 dark:text-slate-300 md:text-base md:tracking-tight"
+          >
+            {{ degree.school }}
+          </div>
+          <div
+            class="m-1 whitespace-pre-wrap p-1 text-justify text-sm tracking-tighter text-blue-900/95 dark:text-slate-300 md:text-base md:tracking-tight"
+          >
+            {{ degree.course }}
+          </div>
+          <div class="w-1/2">
+            <TheProgressBar :limit="degree.precentage" />
+          </div>
+          <div
+            v-if="degree.incomplete"
+            class="m-1 whitespace-pre-wrap p-1 text-justify text-xs tracking-tighter text-blue-900/95 dark:text-slate-400 md:text-base md:tracking-tight"
+          >
+            {{ $t("incomplete") }} - {{ degree.incomplete }}
+          </div>
+        </div>
+      </div>
     </div>
   </div>
 </template>
