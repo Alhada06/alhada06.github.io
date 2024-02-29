754794ed824bd417d366fc149521c26b9680017e
Diogo Alhada <alhada06@gmail.com>
2024-02-29 01:45:23 +0000

    test loadSlim on prod

--- plugins/vue3-particles.client.ts
+++ plugins/vue3-particles.client.ts
@@ -1,11 +1,11 @@
 import Particles from "@tsparticles/vue3";
 import { loadFull } from "tsparticles";
-
+import { loadSlim } from "@tsparticles/slim";
 export default defineNuxtPlugin((nuxtApp) => {
   // Doing something with nuxtApp
   nuxtApp.vueApp.use(Particles, {
     init: async (engine) => {
-      await loadFull(engine);
+      await loadSlim(engine);
     },
   });
 });
