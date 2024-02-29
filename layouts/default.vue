fa26c7a873e0b94ccecaf3dfe0091a8a4a040aea
Diogo Alhada <alhada06@gmail.com>
2024-02-29 22:12:22 +0000

    optimized particles for themes

--- layouts/default.vue
+++ layouts/default.vue
@@ -12,26 +12,61 @@ const { stop } = useIntersectionObserver(
     // console.log(observerElement)
   },
 );
-// const particlesContainer = ref(null);
-// const particlesInit = async (engine: any) => {
-//   await loadFull(engine);
-// };
 
-// eslint-disable-next-line @typescript-eslint/no-unused-vars
-const tsparticles = ref(null);
-// const test = () => {
-//   console.log(tsparticles.value, particlesContainer.value);
-//   particlesContainer.value.loadTheme("light");
-// };
+const particlesContainer: Container = ref(undefined);
 const particlesLoaded = (container?: Container) => {
-  console.log(container);
+  container?.loadTheme(isDark.value ? "dark" : "light");
+  particlesContainer.value = container;
+  console.log("trigrred load particles", new Date());
 };
 const options: ISourceOptions = {
-  background: {
-    color: {
-      value: "#041731",
+  themes: [
+    {
+      name: "dark",
+      default: {
+        value: true,
+        mode: "dark",
+      },
+      options: {
+        background: {
+          color: {
+            value: "#041731",
+          },
+        },
+        particles: {
+          color: {
+            value: "#ffffff",
+          },
+          links: {
+            color: "#ffffff",
+          },
+        },
+      },
     },
-  },
+    {
+      name: "light",
+      default: {
+        value: true,
+        mode: "light",
+      },
+      options: {
+        background: {
+          color: {
+            value: "#f1f5f9",
+          },
+        },
+        particles: {
+          color: {
+            value: "#1e40af",
+          },
+          links: {
+            color: "#1e40af",
+          },
+        },
+      },
+    },
+  ],
+
   fullScreen: {
     enable: true,
     zIndex: -1,
@@ -68,11 +103,7 @@ const options: ISourceOptions = {
     },
   },
   particles: {
-    color: {
-      value: "#ffffff",
-    },
     links: {
-      color: "#ffffff",
       distance: 150,
       enable: true,
       opacity: 0.5,
@@ -109,105 +140,101 @@ const options: ISourceOptions = {
   },
   detectRetina: true,
 };
-const optionsLight: ISourceOptions = {
-  background: {
-    color: {
-      value: "#f1f5f9", // slate-100
-    },
-  },
-  fullScreen: {
-    enable: true,
-    zIndex: -1,
-  },
-  fpsLimit: 60,
-  interactivity: {
-    detectsOn: "window",
-    events: {
-      onClick: {
-        enable: true,
-        mode: "push",
-      },
-      onHover: {
-        enable: true,
-        mode: "grab",
-      },
-      // resize: true,
-    },
-    modes: {
-      bubble: {
-        distance: 400,
-        duration: 2,
-        opacity: 0.8,
-        size: 40,
-      },
-      push: {
-        quantity: 4,
-        duration: 0.2,
-      },
-      repulse: {
-        distance: 200,
-        duration: 0.4,
-      },
-    },
-  },
-  particles: {
-    color: {
-      value: "#1e40af",
-    },
-    links: {
-      color: "#1e40af",
-      distance: 150,
-      enable: true,
-      opacity: 0.5,
-      width: 1,
-    },
-    collisions: {
-      enable: true,
-    },
-    move: {
-      direction: "none",
-      enable: true,
-      outModes: "out",
-      random: false,
-      speed: 2,
-      straight: false,
-    },
-    number: {
-      density: {
-        enable: true,
-        // value_area: 1000,
-      },
-      value: 100,
-    },
-    opacity: {
-      value: 0.5,
-    },
-    shape: {
-      type: "circle",
-    },
-    size: {
-      // random: true,
-      value: 3,
-    },
-  },
-  detectRetina: true,
-};
+watch(isDark, () => {
+  particlesContainer.value.loadTheme(isDark.value ? "dark" : "light");
+});
+// const optionsLight: ISourceOptions = {
+//   background: {
+//     color: {
+//       value: "#f1f5f9", // slate-100
+//     },
+//   },
+//   fullScreen: {
+//     enable: true,
+//     zIndex: -1,
+//   },
+//   fpsLimit: 60,
+//   interactivity: {
+//     detectsOn: "window",
+//     events: {
+//       onClick: {
+//         enable: true,
+//         mode: "push",
+//       },
+//       onHover: {
+//         enable: true,
+//         mode: "grab",
+//       },
+//       // resize: true,
+//     },
+//     modes: {
+//       bubble: {
+//         distance: 400,
+//         duration: 2,
+//         opacity: 0.8,
+//         size: 40,
+//       },
+//       push: {
+//         quantity: 4,
+//         duration: 0.2,
+//       },
+//       repulse: {
+//         distance: 200,
+//         duration: 0.4,
+//       },
+//     },
+//   },
+//   particles: {
+//     color: {
+//       value: "#1e40af",
+//     },
+//     links: {
+//       color: "#1e40af",
+//       distance: 150,
+//       enable: true,
+//       opacity: 0.5,
+//       width: 1,
+//     },
+//     collisions: {
+//       enable: true,
+//     },
+//     move: {
+//       direction: "none",
+//       enable: true,
+//       outModes: "out",
+//       random: false,
+//       speed: 2,
+//       straight: false,
+//     },
+//     number: {
+//       density: {
+//         enable: true,
+//         // value_area: 1000,
+//       },
+//       value: 100,
+//     },
+//     opacity: {
+//       value: 0.5,
+//     },
+//     shape: {
+//       type: "circle",
+//     },
+//     size: {
+//       // random: true,
+//       value: 3,
+//     },
+//   },
+//   detectRetina: true,
+// };
 </script>
 <template>
   <div>
     <div class="relative -z-10 h-full w-full bg-bgblue">
       <ClientOnly>
         <vue-particles
-          v-if="isDark"
           id="tsparticles"
           @particles-loaded="particlesLoaded"
           :options="options"
-        />
-        <vue-particles
-          v-else
-          id="tsparticles"
-          @particles-loaded="particlesLoaded"
-          :options="optionsLight"
         />
       </ClientOnly>
     </div>
--- pages/loading/index.vue
+++ pages/loading/index.vue
@@ -1,7 +1,7 @@
 <script setup lang="ts">
-definePageMeta({
-  layout: "backgound",
-});
+// definePageMeta({
+//   layout: "defa",
+// });
 // setTimeout(() => {
 //     return navigateTo("/");
 // }, 2000);
